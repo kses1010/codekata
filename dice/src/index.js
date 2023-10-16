1ef592f9960f9f51a8ed77a2f907e680bb124c74
sunny-son <thstkd92@gmail.com>
2023-10-16 15:58:38 +0900

    chore : 주사위 게임 - 컴포넌트

---
+++ dice/src/index.js
@@ -0,0 +1,9 @@
+// index.js
+import ReactDOM from 'react-dom/client';
+import App from "./App";
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
