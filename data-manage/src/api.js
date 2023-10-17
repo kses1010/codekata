8a2ca742e5595d5c184f23221a4e3048c9279802
sunny-son <thstkd92@gmail.com>
2023-10-17 15:59:04 +0900

    lecture : 영화 리뷰 - 서버에서 정렬한 데이터를 받아오기

--- data-manage/src/api.js
+++ data-manage/src/api.js
@@ -1,6 +1,7 @@
 const DOMAIN = 'https://learn.codeit.kr/2895';
 
-export async function getReviews() {
-  const response = await fetch(`${DOMAIN}/film-reviews`);
+export async function getReviews(order = 'createdAt') {
+  const query = `order=${order}`;
+  const response = await fetch(`${DOMAIN}/film-reviews?${query}`);
   return await response.json();
 }
