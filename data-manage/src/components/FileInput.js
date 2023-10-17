46d2aa3d63b46873cc897fd2193e4f81b39d6f52
sunny-son <thstkd92@gmail.com>
2023-10-17 20:25:57 +0900

    lecture : 영화 리뷰 - ref로 DOM 노드 가져오기

--- data-manage/src/components/FileInput.js
+++ data-manage/src/components/FileInput.js
@@ -1,11 +1,21 @@
+import { useEffect, useRef } from 'react';
+
 function FileInput({name, value, onChange}) {
+  const inputRef = useRef();
+
   const handleChange = (e) => {
     const nextValue = e.target.files[0];
     onChange(name, nextValue);
   };
 
+  useEffect(() => {
+    if (inputRef.current) {
+      console.log(inputRef);
+    }
+  }, []);
+
   return (
-    <input type='file' onChange={handleChange} />
+    <input type='file' onChange={handleChange} ref={inputRef}/>
   );
 }
 
