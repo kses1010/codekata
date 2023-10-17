3f8bce8a22d522eb2fc22aaafd1d00f58d6d898c
sunny-son <thstkd92@gmail.com>
2023-10-17 16:39:29 +0900

    lecture : 영화 리뷰 - 데이터 더 불러오기(페이지네이션)

--- data-manage/src/api.js
+++ data-manage/src/api.js
@@ -1,7 +1,7 @@
 const DOMAIN = 'https://learn.codeit.kr/2895';
 
-export async function getReviews(order = 'createdAt') {
-  const query = `order=${order}`;
+export async function getReviews({order = 'createdAt', offset = 0, limit = 6}) {
+  const query = `order=${order}&offset=${offset}&limit=${limit}`;
   const response = await fetch(`${DOMAIN}/film-reviews?${query}`);
   return await response.json();
 }
