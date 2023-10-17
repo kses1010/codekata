8aaf92c75167631030b35e7abe91746940016da6
sunny-son <thstkd92@gmail.com>
2023-10-17 18:34:42 +0900

    lecture : 영화 리뷰 - 네트워크 로딩

--- data-manage/src/components/App.js
+++ data-manage/src/components/App.js
@@ -9,6 +9,7 @@ function App() {
   const [order, setOrder] = useState('createdAt');
   const [offset, setOffset] = useState(0);
   const [hasNext, setHasNext] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
@@ -20,7 +21,18 @@ function App() {
   };
 
   const handleLoad = async (options) => {
-    const { reviews, paging } = await getReviews(options);
+    let result;
+    try {
+      setIsLoading(true);
+      result = await getReviews(options);
+    } catch (err) {
+      console.error(err);
+      return;
+    } finally {
+      setIsLoading(false);
+    }
+
+    const { reviews, paging } = result;
     if (options.offset === 0) {
       setItems(reviews);
     } else {
@@ -45,7 +57,7 @@ function App() {
         <button onClick={handleBestClick}>베스트순</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
-      {hasNext && <button onClick={handleLoadMore}>더 보기</button>}
+      {hasNext && <button disabled={isLoading} onClick={handleLoadMore}>더 보기</button>}
     </div>
   );
 }
