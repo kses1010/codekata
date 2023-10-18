ea3af75e2337f8987df825dd9b5d0172049b6fb4
sunny-son <thstkd92@gmail.com>
2023-10-18 11:53:46 +0900

    lecture : 영화 리뷰 - 나만의 Hook으로 코드 정리하기

--- data-manage/src/components/App.js
+++ data-manage/src/components/App.js
@@ -2,6 +2,7 @@ import ReviewList from './ReviewList';
 import { useEffect, useState } from 'react';
 import { createReview, deleteReview, getReviews, updateReview } from '../api';
 import ReviewForm from './ReviewForm';
+import useAsync from '../hooks/useAsync';
 
 const LIMIT = 6;
 
@@ -10,8 +11,7 @@ function App() {
   const [order, setOrder] = useState('createdAt');
   const [offset, setOffset] = useState(0);
   const [hasNext, setHasNext] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const [loadingError, setLoadingError] = useState(null);
+  const [isLoading, loadingError, getReviewsAsync] = useAsync(getReviews);
 
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
@@ -28,16 +28,9 @@ function App() {
   };
 
   const handleLoad = async (options) => {
-    let result;
-    try {
-      setIsLoading(true);
-      setLoadingError(null);
-      result = await getReviews(options);
-    } catch (err) {
-      setLoadingError(err);
+    let result = await getReviewsAsync(options);
+    if (!result) {
       return;
-    } finally {
-      setIsLoading(false);
     }
 
     const { reviews, paging } = result;
--- data-manage/src/components/ReviewForm.js
+++ data-manage/src/components/ReviewForm.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import FileInput from './FileInput';
 import RatingInput from './RatingInput';
 import './ReviewForm.css';
+import useAsync from '../hooks/useAsync';
 
 const INITIAL_VALUES = {
   title: '',
@@ -18,8 +19,7 @@ function ReviewForm({
                       onCancel,
                     }) {
   const [values, setValues] = useState(initialValues);
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const [submittingError, setSubmittingError] = useState(null);
+  const [isSubmitting, submittingError, onSubmitAsync] = useAsync(onSubmit);
 
   const handleChange = (name, value) => {
     setValues((prevValues) => ({
@@ -41,16 +41,9 @@ function ReviewForm({
     formData.append('content', values.content);
     formData.append('imgFile', values.imgFile);
 
-    let result;
-    try {
-      setSubmittingError(null);
-      setIsSubmitting(true);
-      result = await onSubmit(formData);
-    } catch (error) {
-      setSubmittingError(error);
+    let result = await onSubmitAsync(formData);
+    if (!result) {
       return;
-    } finally {
-      setIsSubmitting(false);
     }
     const { review } = result;
     setValues(INITIAL_VALUES);
