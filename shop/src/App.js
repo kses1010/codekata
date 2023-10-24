9d03f14a2ab459d6c26bb5c3f0f0e5849d6f131f
sunny-son <thstkd92@gmail.com>
2023-10-24 18:25:09 +0900

    lecture : 코딩애플 - 쇼핑몰 만들기: styled-components 사용하기

--- shop/src/App.js
+++ shop/src/App.js
@@ -17,7 +17,7 @@ function App() {
           <Navbar.Brand href='#home'>Shop</Navbar.Brand>
           <Nav className='me-auto'>
             <Nav.Link onClick={() => navigate('/')}>Home</Nav.Link>
-            <Nav.Link onClick={() => navigate('/detail')}>Cart</Nav.Link>
+            <Nav.Link onClick={() => navigate('/detail')}>Detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
--- shop/src/pages/DetailPage.jsx
+++ shop/src/pages/DetailPage.jsx
@@ -1,14 +1,33 @@
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+
+const YellowButton = styled.button`
+  background: ${props => props.bg};
+  color: ${props => props.bg === 'blue' ? 'white' : 'black'};
+  padding: 1rem;
+`;
+
+const Box = styled.div`
+  background: grey;
+  padding: 2rem;
+`;
+
+const NewButton = styled.button(YellowButton);
 
 function DetailPage({ shoes }) {
-  const {id} = useParams();
+  const { id } = useParams();
   const shoe = shoes.find((item) => item.id === Number(id));
 
   return (
     <div className='container'>
+      <Box>
+        <YellowButton bg='blue'>버튼</YellowButton>
+        <YellowButton bg='orange'>버튼</YellowButton>
+        <NewButton>버튼</NewButton>
+      </Box>
       <div className='row'>
         <div className='col-md-6'>
-          <img src={`${shoe.image}`} width='100%'  alt=''/>
+          <img src={`${shoe.image}`} width='100%' alt='' />
         </div>
         <div className='col-md-6'>
           <h4 className='pt-5'>{shoe.title}</h4>
