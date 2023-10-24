de3d76fd1f3efbb79d28f0fb5f16948da25cc94e
sunny-son <thstkd92@gmail.com>
2023-10-24 18:45:50 +0900

    lecture : 코딩애플 - 쇼핑몰 만들기: Lifecycle과 useEffect 1

--- shop/src/pages/DetailPage.jsx
+++ shop/src/pages/DetailPage.jsx
@@ -1,30 +1,27 @@
 import { useParams } from 'react-router-dom';
-import styled from 'styled-components';
+import React, { useEffect, useState } from 'react';
 
-const YellowButton = styled.button`
-  background: ${props => props.bg};
-  color: ${props => props.bg === 'blue' ? 'white' : 'black'};
-  padding: 1rem;
-`;
-
-const Box = styled.div`
-  background: grey;
-  padding: 2rem;
-`;
-
-const NewButton = styled.button(YellowButton);
 
 function DetailPage({ shoes }) {
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+  });
+
+  const [alert, setAlert] = useState(true);
   const { id } = useParams();
   const shoe = shoes.find((item) => item.id === Number(id));
 
   return (
     <div className='container'>
-      <Box>
-        <YellowButton bg='blue'>버튼</YellowButton>
-        <YellowButton bg='orange'>버튼</YellowButton>
-        <NewButton>버튼</NewButton>
-      </Box>
+      {alert === true ?
+        <div className='alert alert-warning'>
+          2초 이내 구매시 할인
+        </div>
+        : null
+      }
       <div className='row'>
         <div className='col-md-6'>
           <img src={`${shoe.image}`} width='100%' alt='' />
