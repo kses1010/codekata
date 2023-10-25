474a278aab7235987a67cabda37971d19abb38e4
sunny-son <thstkd92@gmail.com>
2023-10-25 15:08:43 +0900

    lecture : 코딩애플 - 쇼핑몰 만들기: 탭 UI

--- shop/src/pages/DetailPage.jsx
+++ shop/src/pages/DetailPage.jsx
@@ -1,11 +1,13 @@
 import { useParams } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
-
+import { Nav } from 'react-bootstrap';
 
 function DetailPage({ shoes }) {
 
   const [alert, setAlert] = useState(true);
   const { id } = useParams();
+  const [tap, setTap] = useState(0);
+
   const shoe = shoes.find((item) => item.id === Number(id));
 
   useEffect(() => {
@@ -38,8 +40,31 @@ function DetailPage({ shoes }) {
           <button className='btn btn-danger'>주문하기</button>
         </div>
       </div>
+
+      <Nav variant='tabs' defaultActiveKey='link0'>
+        <Nav.Item>
+          <Nav.Link onClick={() => {
+            setTap(0)
+          }} eventKey='link0'>버튼0</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link onClick={() => {
+            setTap(1)
+          }} eventKey='link1'>버튼1</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link onClick={() => {
+            setTap(2)
+          }} eventKey='link2'>버튼2</Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <TabContent tap={tap}/>
     </div>
   );
+}
+
+function TabContent({ tap }) {
+  return [<div>내용0</div>, <div>내용1</div>, <div>내용2</div>][tap];
 }
 
 export default DetailPage;
