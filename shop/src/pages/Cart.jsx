7ca2d0afcd50a3a81949438ad950e3340c401086
sunny-son <thstkd92@gmail.com>
2023-10-25 19:48:58 +0900

    lecture : 코딩애플 - 쇼핑몰 만들기: Redux - object/array 상태값 변경하기

--- shop/src/pages/Cart.jsx
+++ shop/src/pages/Cart.jsx
@@ -1,27 +1,45 @@
 import { Table } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeName, increase } from '../redux/useSlice';
 
 function Cart() {
 
+  const user = useSelector((state) => state.user);
+  const stock = useSelector((state) => state.stock);
+  const disPatch = useDispatch();
 
   return (
-    <Table>
-      <thead>
-      <tr>
-        <th>#</th>
-        <th>상품명</th>
-        <th>수량</th>
-        <th>변경하기</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr>
-        <td>1</td>
-        <td>안녕</td>
-        <td>안녕</td>
-        <td>안녕</td>
-      </tr>
-      </tbody>
-    </Table>
+    <div>
+      <h4>{user.name} {user.age}의 장바구니</h4>
+      <button onClick={() => {
+        disPatch(increase(10));
+      }}>버튼</button>
+      <Table>
+        <thead>
+        <tr>
+          <th>#</th>
+          <th>상품명</th>
+          <th>수량</th>
+          <th>변경하기</th>
+        </tr>
+        </thead>
+        <tbody>
+        {stock.map((item) =>
+          <tr key={item.id}>
+            <td>{item.id}</td>
+            <td>{item.name}</td>
+            <td>{item.count}</td>
+            <td>
+              <button onClick={() => {
+                disPatch(changeName());
+              }}>+
+              </button>
+            </td>
+          </tr>,
+        )}
+        </tbody>
+      </Table>
+    </div>
   );
 }
 
--- shop/src/redux/Store.js
+++ shop/src/redux/Store.js
@@ -1,7 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+import user from './useSlice';
+
+
+const stock = createSlice({
+  name: 'stock',
+  initialState: [
+    { id: 0, name: 'White and Black', count: 2 },
+    { id: 2, name: 'Grey Yordan', count: 1 },
+  ],
+});
 
 export default configureStore({
   reducer: {
-
-  }
+    user: user.reducer,
+    stock: stock.reducer,
+  },
 });
