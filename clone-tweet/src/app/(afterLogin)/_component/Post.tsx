234bd24472c59c940230ef2cdca261d2cd240ca9
sunny-son <thstkd92@gmail.com>
2023-12-08 18:47:08 +0900

    lecture : Next + React Query - classnames로 클래스 합성하기

--- clone-tweet/src/app/(afterLogin)/_component/Post.tsx
+++ clone-tweet/src/app/(afterLogin)/_component/Post.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import 'dayjs/locale/ko';
-// import ActionButtons from "@/app/(afterLogin)/_component/ActionButtons";
+import ActionButtons from '@/app/(afterLogin)/_component/ActionButtons';
 
 dayjs.locale('ko');
 dayjs.extend(relativeTime)
@@ -45,7 +45,7 @@ export default function Post() {
           <div>{target.content}</div>
           <div className={style.postImageSection}>
           </div>
-          {/*<ActionButtons />*/}
+          <ActionButtons />
         </div>
       </div>
     </article>
