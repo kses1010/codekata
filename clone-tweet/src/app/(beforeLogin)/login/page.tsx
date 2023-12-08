70611037dd83808679228930e821299581e4b70f
sunny-son <thstkd92@gmail.com>
2023-12-08 14:26:31 +0900

    lecture : Next + React Query - Main, SignupModal css 설명

--- clone-tweet/src/app/(beforeLogin)/login/page.tsx
+++ clone-tweet/src/app/(beforeLogin)/login/page.tsx
@@ -1,11 +1,8 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import styles from '@/app/page.module.css';
-import Image from 'next/image';
-import zLogo from '/public/zlogo.png';
-import Link from 'next/link';
 import { useEffect } from 'react';
+import Main from '@/app/(beforeLogin)/_component/Main';
 
 export default function Login() {
   const router = useRouter();
@@ -14,17 +11,6 @@ export default function Login() {
   }, [router]);
 
   return (
-    <>
-      <div className={styles.left}>
-        <Image src={zLogo} alt='Z로고' />
-      </div>
-      <div className={styles.right}>
-        <h1>지금 일어나고 있는 일</h1>
-        <h2>지금 가입하세요.</h2>
-        <Link href='/i/flow/sign-up' className={styles.signup}>계정 만들기</Link>
-        <h3>이미 트위터에 가입하셨나요?</h3>
-        <Link href='/login' className={styles.login}>로그인</Link>
-      </div>
-    </>
+    <Main />
   );
 }
