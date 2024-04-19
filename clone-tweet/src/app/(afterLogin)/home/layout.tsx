86ad5949406c4251aff02c7a1c615a0be49f4481
sunny-son <thstkd92@gmail.com>
2024-04-19 22:32:23 +0900

    lecture : Next + React Query - 라우트 그룹

---
+++ clone-tweet/src/app/(afterLogin)/home/layout.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from 'react';
+
+export default async function Layout({ children }: { children: ReactNode }) {
+  return (
+    <div>
+      홈 레이아웃
+      {children}
+    </div>
+  );
+}
--- clone-tweet/src/app/home/layout.tsx
+++
@@ -1,7 +0,0 @@
-import {ReactNode} from "react";
-
-export default async function Layout({children}: { children: ReactNode }) {
-  return (
-    <div>{children}</div>
-  );
-}
