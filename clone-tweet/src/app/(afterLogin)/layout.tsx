0f105942819f4d05aeeb7c18ade7c4c0932a6597
sunny-son <thstkd92@gmail.com>
2024-05-03 23:23:50 +0900

    lecture : Next + React Query - react-query SSR 설정하기

--- clone-tweet/src/app/(afterLogin)/layout.tsx
+++ clone-tweet/src/app/(afterLogin)/layout.tsx
@@ -9,6 +9,7 @@ import TrendSection from '@/app/(afterLogin)/_component/TrendSection';
 import FollowRecommend from '@/app/(afterLogin)/_component/FollowRecommend';
 import RightSearchZone from '@/app/(afterLogin)/_component/RightSearchZone';
 import { auth } from '@/auth';
+import RQProvider from '@/app/(afterLogin)/_component/RQProvider';
 
 type Props = { children: ReactNode; modal: ReactNode };
 
@@ -19,7 +20,7 @@ export default async function AfterLoginLayout({ children, modal }: Props) {
       <header className={style.leftSectionWrapper}>
         <section className={style.leftSection}>
           <div className={style.leftSectionFixed}>
-            <Link className={style.logo} href={session?.user ? "/home" : '/'}>
+            <Link className={style.logo} href={session?.user ? '/home' : '/'}>
               <div className={style.logoPill}>
                 <Image src={ZLogo} alt="z.com로고" width={40} height={40} />
               </div>
@@ -49,22 +50,24 @@ export default async function AfterLoginLayout({ children, modal }: Props) {
           </div>
         </section>
       </header>
-      <div className={style.rightSectionWrapper}>
-        <div className={style.rightSectionInner}>
-          <main className={style.main}>{children}</main>
-          <section className={style.rightSection}>
-            <RightSearchZone />
-            <TrendSection />
-            <div className={style.followRecommend}>
-              <h3>팔로우 추천</h3>
-              <FollowRecommend />
-              <FollowRecommend />
-              <FollowRecommend />
-            </div>
-          </section>
+      <RQProvider>
+        <div className={style.rightSectionWrapper}>
+          <div className={style.rightSectionInner}>
+            <main className={style.main}>{children}</main>
+            <section className={style.rightSection}>
+              <RightSearchZone />
+              <TrendSection />
+              <div className={style.followRecommend}>
+                <h3>팔로우 추천</h3>
+                <FollowRecommend />
+                <FollowRecommend />
+                <FollowRecommend />
+              </div>
+            </section>
+          </div>
         </div>
-      </div>
-      {modal}
+        {modal}
+      </RQProvider>
     </div>
   );
 }
