731abd2d365019ca66cbafe0355b6641e433bf97
sunny-son <thstkd92@gmail.com>
2024-05-04 20:40:58 +0900

    lecture : Next + React Query - 에러 상황 처리하기

---
+++ clone-tweet/src/app/(afterLogin)/[username]/_component/UserPosts.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { Post as IPost } from '@/model/Post';
+import Post from '@/app/(afterLogin)/_component/Post';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { getUserPosts } from '@/app/(afterLogin)/[username]/_lib/getUserPosts';
+
+type Props = { username: string };
+
+export default function UserPosts({ username }: Props) {
+  const { data } = useQuery<IPost[], object, IPost[], [_1: string, _2: string, _3: string]>({
+    queryKey: ['posts', 'users', username],
+    queryFn: getUserPosts,
+    staleTime: 60 * 1000, // fresh -> stale
+    gcTime: 5 * 60 * 1000, // 기본값이 5분이다.
+  });
+
+  const queryClient = useQueryClient();
+  const user = queryClient.getQueryData(['users', username]);
+
+  console.log(user);
+  if (user) {
+    return (
+      <>
+        {data?.map((post) => {
+          return <Post key={post.postId} post={post} />;
+        })}
+      </>
+    );
+  }
+
+  return null;
+}
