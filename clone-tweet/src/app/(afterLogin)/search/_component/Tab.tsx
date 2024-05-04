8bd7481af6cf1deda6a613c65ee8f4c2dd6f7e02
sunny-son <thstkd92@gmail.com>
2024-05-04 21:38:36 +0900

    lecture : Next + React Query - 미흡한 부분 구현하기

--- clone-tweet/src/app/(afterLogin)/search/_component/Tab.tsx
+++ clone-tweet/src/app/(afterLogin)/search/_component/Tab.tsx
@@ -8,13 +8,19 @@ export default function Tab() {
   const [current, setCurrent] = useState('hot');
   const router = useRouter();
   const searchParams = useSearchParams();
+
   const onClickHot = () => {
     setCurrent('hot');
-    router.replace(`/search?q=${searchParams.get('q')}`);
+    const newSearchParams = new URLSearchParams(searchParams);
+    newSearchParams.delete('f');
+    router.replace(`/search?${newSearchParams.toString()}`);
   };
+
   const onClickNew = () => {
     setCurrent('new');
-    router.replace(`/search?${searchParams.toString()}&f=live`);
+    const newSearchParams = new URLSearchParams(searchParams);
+    newSearchParams.set('f', 'live');
+    router.replace(`/search?${newSearchParams.toString()}`);
   };
 
   return (
