8e59f8055c572da099410fafb7875f32cfbf5719
sunny-son <thstkd92@gmail.com>
2024-05-04 21:59:17 +0900

    lecture : Next + React Query - 인피니트 스크롤링

--- clone-tweet/src/app/(afterLogin)/home/_component/PostRecommends.tsx
+++ clone-tweet/src/app/(afterLogin)/home/_component/PostRecommends.tsx
@@ -1,23 +1,30 @@
 'use client';
 
-import { useQuery } from '@tanstack/react-query';
+import { InfiniteData, useInfiniteQuery } from '@tanstack/react-query';
 import { getPostRecommends } from '@/app/(afterLogin)/home/_lib/getPostRecommends';
 import Post from '@/app/(afterLogin)/_component/Post';
 import { Post as IPost } from '@/model/Post';
+import { Fragment } from 'react';
 
 export default function PostRecommends() {
-  const { data } = useQuery<IPost[]>({
+  const { data } = useInfiniteQuery<IPost[], Object, InfiniteData<IPost[]>, [_1: string, _2: string], number>({
     queryKey: ['posts', 'recommends'],
     queryFn: getPostRecommends,
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => lastPage.at(-1)?.postId,
     staleTime: 60 * 1000, // fresh -> stale
     gcTime: 5 * 60 * 1000, // 기본값이 5분이다.
   });
 
   return (
     <>
-      {data?.map((post) => {
-        return <Post key={post.postId} post={post} />;
-      })}
+      {data?.pages.map((page, i) => (
+        <Fragment key={i}>
+          {page.map((post) => (
+            <Post key={post.postId} post={post} />
+          ))}
+        </Fragment>
+      ))}
     </>
   );
 }
