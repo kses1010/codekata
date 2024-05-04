0432b58f55e6e7ad08d54096fdf74d83cbec9b58
sunny-son <thstkd92@gmail.com>
2024-05-04 15:36:24 +0900

    lecture : Next + React Query - react-query를 쓰는 이유와 fresh, stale, inactive

--- clone-tweet/src/app/(afterLogin)/home/_component/PostRecommends.tsx
+++ clone-tweet/src/app/(afterLogin)/home/_component/PostRecommends.tsx
@@ -6,7 +6,12 @@ import Post from '@/app/(afterLogin)/_component/Post';
 import { Post as IPost } from '@/model/Post';
 
 export default function PostRecommends() {
-  const { data } = useQuery<IPost[]>({ queryKey: ['posts', 'recommends'], queryFn: getPostRecommends });
+  const { data } = useQuery<IPost[]>({
+    queryKey: ['posts', 'recommends'],
+    queryFn: getPostRecommends,
+    staleTime: 60 * 1000, // fresh -> stale
+    gcTime: 5 * 60 * 1000, // 기본값이 5분이다.
+  });
 
   return (
     <>
