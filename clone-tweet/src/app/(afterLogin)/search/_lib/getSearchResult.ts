86506d248e17dd60a34c70a2aaa8a53d7d50c845
sunny-son <thstkd92@gmail.com>
2024-05-07 16:22:40 +0900

    lecture : Next + React Query - 주소에 해시가 들어가면 문제가 됩니다

--- clone-tweet/src/app/(afterLogin)/search/_lib/getSearchResult.ts
+++ clone-tweet/src/app/(afterLogin)/search/_lib/getSearchResult.ts
@@ -14,16 +14,14 @@ export const getSearchResult: QueryFunction<
   ]
 > = async ({ queryKey }) => {
   const [_1, _2, searchParams] = queryKey;
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/search/${searchParams.q}?=${searchParams.toString()}`,
-    {
-      next: {
-        tags: ['posts', 'search', searchParams.q],
-      },
-      credentials: 'include',
-      cache: 'no-store',
+  const urlSearchParams = new URLSearchParams(searchParams);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/posts?${urlSearchParams.toString()}`, {
+    next: {
+      tags: ['posts', 'search', searchParams.q],
     },
-  );
+    credentials: 'include',
+    cache: 'no-store',
+  });
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
 
