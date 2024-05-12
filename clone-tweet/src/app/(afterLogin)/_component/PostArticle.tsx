9c619ac99249f10983f5e55642c4cf23adf34b40
sunny-son <thstkd92@gmail.com>
2024-05-12 19:31:58 +0900

    lecture : Next + React Query - 답글과 재게시글 꾸미기

--- clone-tweet/src/app/(afterLogin)/_component/PostArticle.tsx
+++ clone-tweet/src/app/(afterLogin)/_component/PostArticle.tsx
@@ -12,8 +12,13 @@ type Props = {
 
 export default function PostArticle({ children, post }: Props) {
   const router = useRouter();
+  let target = post;
+  if (post.Original) {
+    target = post.Original;
+  }
+  
   const onClick = () => {
-    router.push(`/${post.User.id}/status/${post.postId}`);
+    router.push(`/${target.User.id}/status/${target.postId}`);
   };
 
   return (
