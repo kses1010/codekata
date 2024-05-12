b07f865f61af6a77141ebecea1da8d0d8b6f8826
sunny-son <thstkd92@gmail.com>
2024-05-12 19:13:27 +0900

    lecture : Next + React Query - 완전히 로그아웃 하기 & 프론트 서버에서 쿠키 보내기

--- clone-tweet/src/app/(afterLogin)/[username]/page.tsx
+++ clone-tweet/src/app/(afterLogin)/[username]/page.tsx
@@ -4,8 +4,8 @@ import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query
 import UserPosts from '@/app/(afterLogin)/[username]/_component/UserPosts';
 import UserInfo from '@/app/(afterLogin)/[username]/_component/UserInfo';
 import { getUserPosts } from '@/app/(afterLogin)/[username]/_lib/getUserPosts';
-import { getUser } from '@/app/(afterLogin)/[username]/_lib/getUser';
 import { auth } from '@/auth';
+import { getUserServer } from '@/app/(afterLogin)/[username]/_lib/getUserServer';
 
 type Props = {
   params: { username: string };
@@ -15,7 +15,7 @@ export default async function Profile({ params }: Props) {
   const { username } = params;
   const session = await auth();
   const queryClient = new QueryClient();
-  await queryClient.prefetchQuery({ queryKey: ['users', username], queryFn: getUser });
+  await queryClient.prefetchQuery({ queryKey: ['users', username], queryFn: getUserServer });
   await queryClient.prefetchQuery({ queryKey: ['posts', 'users', username], queryFn: getUserPosts });
   const dehydratedState = dehydrate(queryClient);
 
