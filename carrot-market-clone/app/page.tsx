03fe82bbb367c7bcaa61cd119507b384704df917
sunny-son <thstkd92@gmail.com>
2024-07-13 22:18:55 +0900

    lecture: carrot market clone - Responsive Modifiers

--- carrot-market-clone/app/page.tsx
+++ carrot-market-clone/app/page.tsx
@@ -1,13 +1,13 @@
 export default function Home() {
   return (
-    <main className="bg-gray-100 h-screen flex items-center justify-center p-5 dark:bg-gray-700">
-      <div className="flex flex-col gap-2 bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm dark:bg-gray-600">
+    <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-200 h-screen flex items-center justify-center p-5 dark:bg-gray-700">
+      <div className="flex flex-col gap-2 bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm dark:bg-gray-600 md:flex-row">
         <input
           className="w-full rounded-full h-12 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offest-2 transition-shadow placeholder:drop-shadow"
           type="text"
           placeholder="Search here..."
         />
-        <button className="bg-black bg-opacity-50 text-white py-2 rounded-full outline-none focus:scale-90 active:scale-90 transition-transform font-medium">
+        <button className="bg-black text-white py-2 rounded-full outline-none focus:scale-90 active:scale-90 transition-transform font-medium md:px-10">
           Search
         </button>
       </div>
