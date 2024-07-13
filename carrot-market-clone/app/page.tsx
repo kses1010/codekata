c3439b86910fdf2b14ffec0b2ba5cddf6afc4e54
sunny-son <thstkd92@gmail.com>
2024-07-13 23:30:26 +0900

    lecture: carrot market clone - Group Modifiers

--- carrot-market-clone/app/page.tsx
+++ carrot-market-clone/app/page.tsx
@@ -2,18 +2,11 @@ export default function Home() {
   return (
     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-200 h-screen flex items-center justify-center p-5 dark:bg-gray-700">
       <div className="flex flex-col bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm dark:bg-gray-600 gap-3">
-        {['Nico', 'Sunny', 'You', 'faker'].map((person, index) => (
-          <div key={index} className="flex items-center gap-5">
-            <div className="size-7 bg-blue-400 rounded-full" />
-            {/*<div className="w-40 h-5 rounded-full bg-gray-400" />*/}
-            {/*<div className="w-20 h-5 rounded-full bg-gray-400" />*/}
-            <div className="text-lg font-medium">{person}</div>
-            <div className="relative size-6 bg-red-500 text-white flex items-center justify-center rounded-full">
-              <span>{index + 1}</span>
-              <div className="size-6 bg-red-500 rounded-full absolute animate-ping" />
-            </div>
-          </div>
-        ))}
+        <div className="group">
+          <input type="text" placeholder="이메일을 적어주세요" className="bg-gray-100 w-full" />
+          <span className="group-focus-within:block hidden">이메일 형식인지 확인해주세요.</span>
+        </div>
+        <button>등록</button>
       </div>
     </main>
   );
