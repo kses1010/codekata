16a614bce2015491d9d8a10168e5751c97f5612e
sunny-son <thstkd92@gmail.com>
2024-07-14 15:22:51 +0900

    lecture: carrot market clone - Authentication UI: Log in Screen

--- carrot-market-clone/app/login/page.tsx
+++ carrot-market-clone/app/login/page.tsx
@@ -2,19 +2,17 @@ import FormInput from '@/components/FormInput';
 import FormButton from '@/components/FormBtn';
 import SocialLogin from '@/components/SocialLogin';
 
-export default function CreateAccount() {
+export default function Login() {
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
-        <h2 className="text-xl">Fill in the form belong to join!</h2>
+        <h2 className="text-xl">Log in with email and password.</h2>
       </div>
       <form className="flex flex-col gap-3">
-        <FormInput type="text" placeholder="Username" required errors={[]} />
         <FormInput type="email" placeholder="Email" required errors={[]} />
         <FormInput type="password" placeholder="Password" required errors={[]} />
-        <FormInput type="password" placeholder="Confirm Password" required errors={[]} />
-        <FormButton loading={false} text="Create Account" />
+        <FormButton loading={false} text="Login Account" />
       </form>
       <SocialLogin />
     </div>
